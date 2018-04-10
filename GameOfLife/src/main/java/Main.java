8a3f0a9bcb567c247c09c1aaea49dcc0a53c5da3
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2018-04-10 09:28:17 +0200

    Inital commit

---
+++ GameOfLife/src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+import dot.empire.gol.FrameMain;
+
+/**
+ * Main. Created on 09 Apr 2018 5:02:59 PM by siD.
+ *
+ * @author siD
+ */
+public class Main {
+
+    /**
+     * @param args Arguments from the command line
+     */
+    public static void main(String[] args) {
+        new FrameMain(null).setVisible(true);
+    }
+}
